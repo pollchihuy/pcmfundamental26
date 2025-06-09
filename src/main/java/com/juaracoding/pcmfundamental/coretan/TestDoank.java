c165bcdcbabcba45c1738f9efde3e530fa40cb97
pollchihuy <poll.chihuy@gmail.com>
2025-06-09 20:43:46 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/pcmfundamental/coretan/TestDoank.java
@@ -0,0 +1,19 @@
+package com.juaracoding.pcmfundamental.coretan;
+
+/*
+IntelliJ IDEA 2024.1.4 (Ultimate Edition)
+Build #IU-241.18034.62, built on June 21, 2024
+@Author pollc a.k.a. Paul Christian
+Java Developer
+Created on 09/06/2025 19:30
+@Last Modified 09/06/2025 19:30
+Version 1.0
+*/
+
+/**
+ *  class untuk ujian praktikum 1
+ */
+public class TestDoank {
+
+
+}
